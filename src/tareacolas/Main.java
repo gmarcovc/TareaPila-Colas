b42524f535469a94528a324d638f26c8b94486f8
gmarcovc <gmarcovc@gmail.com>
2023-02-21 21:57:36 -0600

    Elimiinar Pila

---
+++ src/tareacolas/Main.java
@@ -0,0 +1,33 @@
+package tareacolas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cola cola = new Cola();
+        System.out.println("COLA");
+
+        cola.encola(new NodoCola
+        (new Persona(901320952, "Gian", false)));
+        cola.encola(new NodoCola
+        (new Persona(209390242, "Javier", true)));
+        cola.encola(new NodoCola
+        (new Persona(102340543, "Steven", false)));
+        cola.encola(new NodoCola
+        (new Persona(304850485, "Maria", false)));
+
+        System.out.print(cola.imprimir());
+        
+        System.out.print("\n");
+        
+        System.out.println(
+                cola.encuentra(901320952)); //deberia de ser true
+        System.out.println(
+                cola.encuentra(209390242)); //deberia de ser true
+        System.out.println(
+                cola.encuentra(387367892)); //deberia de ser false
+
+        
+    }
+
+}
